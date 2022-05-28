d0e6f798daa47fdfbd24c55b48a5e684a29b39d5
javierpb275 <javierpb275@gmail.com>
2022-05-28 17:43:51 +0200

    advanced request parsing with flask-restful

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -33,7 +33,14 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'price',
+            type=float,
+            required=True,
+            help="This field cannot be left blank!"
+        )
+        data = parser.parse_args()
         item = next(filter(lambda some_item: some_item['name'] == name, items), None)
         if item is None:
             item = {
